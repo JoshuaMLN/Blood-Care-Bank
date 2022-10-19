01142d2c935ba05e459b67699858d7fb8f90a534
DiegoMomo <diegoaxel.cruces@unmsm.edu.pe>
2022-10-19 16:12:48 -0500

    Por favor funciona

--- Desarrollo/SABS/Fuentes/src/Main/SABS.java
+++ Desarrollo/SABS/Fuentes/src/Main/SABS.java
@@ -1,11 +1,11 @@
 
 package Main;
 
-
 public class SABS {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("prueba 1");
         System.out.println("Prueba Sanchez");
+        System.out.println("Prueba Zavaleta");
     }
 }
