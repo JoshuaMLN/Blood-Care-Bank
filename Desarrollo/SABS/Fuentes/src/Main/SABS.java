2744e71c3d6bc7d2396ecbbd8569680329468490
RonConCoca123 <dazanu1400@hotmail.com>
2022-10-19 16:25:49 -0500

    holav1

--- Desarrollo/SABS/Fuentes/src/Main/SABS.java
+++ Desarrollo/SABS/Fuentes/src/Main/SABS.java
@@ -8,5 +8,6 @@ public class SABS {
         System.out.println("Prueba Sanchez");
         System.out.println("Prueba Zavaleta");
         System.out.println("Prueba Moore");
+        System.out.println("Prueba Momo1");
     }
 }
