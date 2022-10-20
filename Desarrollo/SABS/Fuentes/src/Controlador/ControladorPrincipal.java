c61065bad6cca6bc45c95ec11fb6734249cadd42
JMozar <jhon.moore@unmsm.edu.pe>
2022-10-19 23:30:25 -0500

    Agregando Controlador Principal

---
+++ Desarrollo/SABS/Fuentes/src/Controlador/ControladorPrincipal.java
@@ -0,0 +1,49 @@
+
+package Controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Modelo.*;
+import Vista.*;
+import Datos.*;
+
+
+public class ControladorPrincipal {
+    private frmPrincipal vista;
+    private Usuario modelo;
+    
+    
+    public ControladorPrincipal(Usuario modelo, frmPrincipal vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+        
+        this.vista.btnDonantes.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+                ControladorRegistrarDonantes controlador = new ControladorRegistrarDonantes (new frmDonantes(), Repositorio.donantes);
+                controlador.iniciar();
+                vista.dispose();
+                }
+            }
+        );
+        
+        this.vista.btnSalir.addActionListener( new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    modelo.salir();
+                    ControladorSistema controlador = new ControladorSistema( Repositorio.usuarios, new frmSistema() );
+                    controlador.iniciar();
+                    vista.dispose();
+                }
+            }
+        );
+
+    } 
+    
+    public void iniciar(){
+        this.vista.setLocationRelativeTo(null);
+        this.vista.setVisible(true);
+        //traer usuario validado y ponerlo en el label
+        this.vista.lblUsuario.setText(modelo.getUsuario());
+    }
+
+    
+}
