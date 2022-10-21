1f205ecb1e570014e99710ef247a70f7a0e4dca8
Seb2103 <jhamir.cespedes@unmsm.edu.pe>
2022-10-20 22:56:50 -0500

    Datos>Repositorio

--- Desarrollo/SABS/Fuentes/src/Main/SABS.java
+++ Desarrollo/SABS/Fuentes/src/Main/SABS.java
@@ -1,8 +1,24 @@
 
 package Main;
 
+import Controlador.ControladorSistema;
+import Datos.Repositorio;
+import Modelo.Empleado;
+import Modelo.Usuario;
+import Vista.frmSistema;
+
 public class SABS {
     public static void main(String[] args) {
-       
+        
+        Repositorio.usuarios.agregar(new Usuario("admin", "123"));
+        Repositorio.usuarios.agregar(new Usuario("user", "000"));       
+        Empleado empleado1= new Empleado("Recepcionista", 1600, "Josefina", "Suarez", "31501974", 954201376);
+        Repositorio.empleados.agregar(empleado1);
+        Empleado empleado2= new Empleado("Conserje", 1500, "Joseph", "Kurao", "74105896", 983511012);
+        Repositorio.empleados.agregar(empleado2);
+        
+        //Inicio de controlador Sistema (Login)
+        ControladorSistema controlador = new ControladorSistema( Repositorio.usuarios, new frmSistema() );
+        controlador.iniciar();
     }
 }
