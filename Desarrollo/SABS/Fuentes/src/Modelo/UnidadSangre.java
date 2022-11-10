bbdf6cba85158acc6a37e8517f575b6289f0a074
JMozar <jhon.moore@unmsm.edu.pe>
2022-11-10 16:58:48 -0500

    Agregando clase unidadSangre

---
+++ Desarrollo/SABS/Fuentes/src/Modelo/UnidadSangre.java
@@ -0,0 +1,43 @@
+
+package Modelo;
+
+
+public class UnidadSangre {
+    private float Volumen;
+    private String GrupoSanguineo;
+    private String Rh;
+
+    public UnidadSangre(float Volumen, String GrupoSanguineo, String Rh) {
+        this.Volumen = Volumen;
+        this.GrupoSanguineo = GrupoSanguineo;
+        this.Rh = Rh;
+    }
+
+    public float getVolumen() {
+        return Volumen;
+    }
+
+    public void setVolumen(float Volumen) {
+        this.Volumen = Volumen;
+    }
+
+    public String getGrupoSanguineo() {
+        return GrupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String GrupoSanguineo) {
+        this.GrupoSanguineo = GrupoSanguineo;
+    }
+
+    public String getRh() {
+        return Rh;
+    }
+
+    public void setRh(String Rh) {
+        this.Rh = Rh;
+    }
+    
+    
+}
+
+
