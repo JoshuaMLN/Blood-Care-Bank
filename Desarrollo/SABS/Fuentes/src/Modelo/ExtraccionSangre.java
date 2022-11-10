ddb37ab1d64e5ebfbf4c79305c91ec99ee9155ac
JMozar <jhon.moore@unmsm.edu.pe>
2022-11-10 16:01:38 -0500

    Creacion clase ExtraccionSangre

---
+++ Desarrollo/SABS/Fuentes/src/Modelo/ExtraccionSangre.java
@@ -0,0 +1,70 @@
+
+package Modelo;
+
+
+public class ExtraccionSangre {
+    private static int contador=0;
+    private int codigo;
+    private String Fecha;
+    private float Volumen;
+    private String GrupoSanguineo;
+    private String Rh;
+    
+
+    private Donante donante;
+
+    public ExtraccionSangre(String Fecha, float Volumen, String GrupoSanguineo, String Rh, Donante donante) {
+        this.codigo=contador;
+        contador++;
+        this.Fecha = Fecha;
+        this.Volumen = Volumen;
+        this.GrupoSanguineo = GrupoSanguineo;
+        this.Rh = Rh;
+        this.donante = donante;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public float getVolumen() {
+        return Volumen;
+    }
+
+    public void setVolumen(float Volumen) {
+        this.Volumen = Volumen;
+    }
+
+    public String getGrupoSanguineo() {
+        return GrupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String GrupoSanguineo) {
+        this.GrupoSanguineo = GrupoSanguineo;
+    }
+
+    public String getRh() {
+        return Rh;
+    }
+
+    public void setRh(String Rh) {
+        this.Rh = Rh;
+    }
+
+    public Donante getDonante() {
+        return donante;
+    }
+
+    public void setDonante(Donante donante) {
+        this.donante = donante;
+    }
+    
+
+    
+    
+
+}
